4239b8ead9e5d812d6e98ae7f7392fab7b9b5483
sisiea <644022748@qq.com>
2017-01-27 20:32:04 +0800

    asd

--- js/result.js
+++ js/result.js
@@ -1,11 +1,22 @@
-    var resultHeader = [];
-    var result = [];
+    var resultHeader = '支付宝口令:';
+    var result = ['早茶社新年快乐','早茶社新年djb','心怀早茶新年大吉','居然全对了'];
 
-    var score = GetQueryString('score');
+    var score = parseInt(GetQueryString('score'));
+    var scoreIndex;
 
-    console.log(score);
+    if(score<15){
+        scoreIndex = 0;
+    }
+    else if(score<20){
+        scoreIndex = 1;
+    }
+    else if(score<23){
+        scoreIndex = 2;
+    }else if(score==23){
+        scoreIndex = 3;
+    }
 
-
+    $('#result').text(resultHeader+result[scoreIndex]);
 
     
 function GetQueryString(name)
